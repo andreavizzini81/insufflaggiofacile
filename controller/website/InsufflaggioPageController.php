a50c553c43bce2e88eef920ca841574b99e4b0d5
andreavizzini81 <33686589+andreavizzini81@users.noreply.github.com>
2026-05-05 11:53:01 +0200

    Revert "Rifocalizza la homepage come landing commerciale per insufflaggio in Sicilia"

--- controller/website/InsufflaggioPageController.php
+++ controller/website/InsufflaggioPageController.php
@@ -5,12 +5,7 @@ class InsufflaggioPageController extends FrontendController {
     public function __invoke() :Response {
         $selected = new StaticPage($this->request->getQueryParam('id'));
         $this->data['staticPage'] = $selected;
-        if ($selected->getViewName() === 'home') {
-            $this->title = 'Insufflaggio cellulosa Sicilia | Isolamento termico casa senza lavori invasivi';
-            $this->description = 'Insufflaggio di cellulosa in Sicilia per isolamento termico casa e coibentazione pareti senza demolizioni. Richiedi preventivo gratuito a Palermo, Catania, Messina e in tutta la Sicilia.';
-        } else {
-            $this->description = $selected->getMetaDescription();
-        }
+        $this->description = $selected->getMetaDescription();
         return $this->render();
     }
 
