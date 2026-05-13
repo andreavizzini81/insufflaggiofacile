a4a21e7710cb45b63305324d394afc6cb36e33c9
andreavizzini81 <33686589+andreavizzini81@users.noreply.github.com>
2026-05-13 08:11:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -549,11 +549,11 @@ $app->router->group('/api', function(&$group) {
         ->setAction('setData');
 
     $group->post('\/seo\-landing\/?')
-        ->setController('SeoLandingPage')
+        ->setController('SeoLanding')
         ->setAction('setData');
 
     $group->delete('\/seo\-landing\/(\d+)\/?')
-        ->setController('SeoLandingPage')
+        ->setController('SeoLanding')
         ->setAction('delete')
         ->setParams(['id' => '@1']);
     
